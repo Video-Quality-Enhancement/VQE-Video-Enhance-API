bb84f99819394fe27ce6de74c5da5466efe99946
Deven-14 <deven.paramaj@gmail.com>
2023-05-06 03:50:25 +0530

    changed from correlationId to requestId

--- internal/video/models/video.model.go
+++ internal/video/models/video.model.go
@@ -2,7 +2,7 @@ package models
 
 type Video struct {
 	Email            string `json:"email" bson:"email"`
-	CorrelationId    string `json:"correlationId" bson:"correlationId"`
+	RequestId        string `json:"requestId" bson:"requestId"` // ? THINK ABOUT THIS
 	UploadedVideoUrl string `json:"uploadedVideoUrl" bson:"uploadedVideoUrl"`
 	EnhancedVideoUrl string `json:"enhancedVideoUrl,omitempty" bson:"enhancedVideoUrl,omitempty"`
 }
