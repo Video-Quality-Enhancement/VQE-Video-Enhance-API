255550f3b7f56dbcb08b7793f95621cb031f8a1d
Deven-14 <deven.paramaj@gmail.com>
2023-05-07 13:58:52 +0530

    added Identify quality function using gocv

--- internal/video/utils/video.util.go
+++ internal/video/utils/video.util.go
@@ -1,11 +1,37 @@
 package utils
 
-import "github.com/Video-Quality-Enhancement/VQE-Backend/internal/video/models"
+import (
+	"log"
 
-// function to identify the quality of video
-func IdentifyQuality(video *models.Video) string {
-	// TODO: implement this function
-	return ""
+	"github.com/Video-Quality-Enhancement/VQE-Backend/internal/video/types"
+	"gocv.io/x/gocv"
+)
+
+func IdentifyQuality(videoUrl string) (string, error) {
+
+	vc, err := gocv.VideoCaptureFile(videoUrl)
+	if err != nil {
+		log.Println("Error opening video capture device: ", videoUrl)
+		return "", err
+	}
+	defer vc.Close()
+
+	var quality types.VideoQuality
+	height := vc.Get(gocv.VideoCaptureFrameHeight)
+
+	switch {
+	case height <= 144:
+		quality = types.Q144p
+	case height <= 240:
+		quality = types.Q240p
+	case height <= 360:
+		quality = types.Q360p
+	case height <= 480:
+		quality = types.Q480p
+	default:
+		quality = types.Q720p
+	}
+
+	return quality.String(), nil
+
 }
-
-// function to create correlation id
