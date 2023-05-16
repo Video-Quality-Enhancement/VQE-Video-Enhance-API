d1ab6cf9baebc825b77c362916b672a39a7d91d1
Deven-14 <deven.paramaj@gmail.com>
2023-05-16 20:33:46 +0530

    changed x-userId to X-User-ID

--- internal/utils/user.id.go
+++ internal/utils/user.id.go
@@ -10,14 +10,14 @@ func SetUserId(c *gin.Context, userId string) {
 	if userId == "" {
 		slog.Warn("User ID missing, cannot set userId")
 	} else {
-		c.Set("x-userId", userId)
+		c.Set("X-User-ID", userId)
 	}
 
 }
 
 func GetUserId(c *gin.Context) string {
 
-	userId := c.GetString("x-userId")
+	userId := c.GetString("X-User-ID")
 
 	if userId == "" {
 		slog.Warn("User ID missing, cannot get userId")
