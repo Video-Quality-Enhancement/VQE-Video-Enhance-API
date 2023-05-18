48a35b05ac015941542aac9e6bf7df4d45a4dcf9
Deven-14 <deven.paramaj@gmail.com>
2023-05-18 13:43:59 +0530

    changed Uploaded to uploaded in json and bson

--- internal/models/video.model.go
+++ internal/models/video.model.go
@@ -4,13 +4,13 @@ type VideoEnhance struct {
 	UserId             string   `json:"userId" bson:"userId"`
 	RequestId          string   `json:"requestId" bson:"requestId"`
 	ResponseInterfaces []string `json:"responseInterfaces" bson:"responseInterfaces"`
-	UploadedVideoUrl   string   `json:"UploadedVideoUrl" bson:"UploadedVideoUrl"`
+	UploadedVideoUrl   string   `json:"uploadedVideoUrl" bson:"uploadedVideoUrl"`
 }
 
 type VideoEnhanceRequest struct {
 	UserId           string `json:"userId" bson:"userId"`
 	RequestId        string `json:"requestId" bson:"requestId"`
-	UploadedVideoUrl string `json:"UploadedVideoUrl" bson:"UploadedVideoUrl"`
+	UploadedVideoUrl string `json:"uploadedVideoUrl" bson:"uploadedVideoUrl"`
 }
 
 // TODO: change uri back to url and apply binding
