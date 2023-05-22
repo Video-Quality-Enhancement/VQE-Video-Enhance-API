94103e2fe6701a6d082e54610f0cdbd2c3b2aa36
Deven-14 <deven.paramaj@gmail.com>
2023-05-22 19:55:51 +0530

    added service name to log file name

--- internal/config/slog.go
+++ internal/config/slog.go
@@ -11,7 +11,8 @@ import (
 func SetupSlogOutputFile() *os.File {
 
 	date := time.Now().Format(time.DateOnly)
-	fileName := "./logs/video_service_" + date + "_log.json"
+	serviceName := os.Getenv("SERVICE_NAME")
+	fileName := "./logs/" + serviceName + "_" + date + "_log.json"
 	logFile, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
