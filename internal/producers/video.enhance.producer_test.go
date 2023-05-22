d2e42cb9f74477b68851ffc400ef36e699fc1cf1
Deven-14 <deven.paramaj@gmail.com>
2023-05-22 19:59:45 +0530

    added test cases

---
+++ internal/producers/video.enhance.producer_test.go
@@ -0,0 +1,31 @@
+package producers_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Video-Quality-Enhancement/VQE-User-Video-API/internal/config"
+	"github.com/Video-Quality-Enhancement/VQE-User-Video-API/internal/models"
+	"github.com/Video-Quality-Enhancement/VQE-User-Video-API/internal/producers"
+)
+
+func TestVideoEnhanceProducer_Publish(t *testing.T) {
+
+	os.Setenv("AMQP_URL", "amqp://guest:guest@localhost:5672/")
+	conn := config.NewAMQPconnection()
+	defer conn.DisconnectAll()
+
+	ch := conn.NewChannel()
+
+	producer := producers.NewVideoEnhanceProducer(ch)
+	err := producer.Publish(&models.VideoEnhanceRequest{
+		UserId:    "1234",
+		RequestId: "123",
+		VideoUrl:  "https://download.samplelib.com/mp4/sample-5s.mp4",
+	})
+
+	if err != nil {
+		t.Error("Failed to publish message", "err", err)
+	}
+
+}
---
+++ internal/utils/video.util_test.go
@@ -0,0 +1,19 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/Video-Quality-Enhancement/VQE-User-Video-API/internal/utils"
+)
+
+func TestIdentifyQuality(t *testing.T) {
+
+	quality, err := utils.IdentifyQuality("https://download.samplelib.com/mp4/sample-5s.mp4")
+
+	t.Log(quality)
+
+	if err != nil {
+		t.Error("Failed to identify quality", "err", err)
+	}
+
+}
